0599fbd2a027d11b7a309ef62950433e3fbfd9f6
wpob <01168909@pw.edu.pl>
2024-05-30 01:06:24 +0200

    Starting selected property changed

--- monopoly_ai/game_elements/src/Board.cc
+++ monopoly_ai/game_elements/src/Board.cc
@@ -2,7 +2,7 @@
 #include "Board.h"
 
 Board::Board(float width, float height, std::shared_ptr<sf::RenderWindow> win)
-    : Drawable(win), current_action_(Action::BUY_PROPERTY), current_player_(0), fieldLoader_(), dice_tossed_(false), selected_property_(5) {
+    : Drawable(win), current_action_(Action::BUY_PROPERTY), current_player_(0), fieldLoader_(), dice_tossed_(false), selected_property_(-1) {
     dice_ = std::make_unique<Dice>(win);
     shape_.setSize(sf::Vector2f(width, height));
     shape_.setFillColor(sf::Color::White);
