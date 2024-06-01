e0a909fd3f999314ce6e001887ecfe94e7f32590
wpob <01168909@pw.edu.pl>
2024-06-01 18:50:08 +0200

    Moved drawSquaresDescription logic to TextRenderer

--- monopoly_ai/game_elements/include/TextRenderer.h
+++ monopoly_ai/game_elements/include/TextRenderer.h
@@ -8,7 +8,8 @@ public:
     TextRenderer(std::shared_ptr<sf::RenderWindow> win);
 
     void renderText(const std::string& text, const sf::Vector2f& position, const sf::Color& color, unsigned int size = 24);
-    void renderPlayers(std::vector<std::shared_ptr<Player>> players, int current_player);
+    void renderPlayers(std::vector<std::shared_ptr<Player>>& players, int current_player);
+    void renderSquaresDescription(std::vector<std::shared_ptr<BoardSquare>>& squares, int selected_property);
 
 private:
     void initializeTexts();
--- monopoly_ai/game_elements/src/TextRenderer.cc
+++ monopoly_ai/game_elements/src/TextRenderer.cc
@@ -1,4 +1,5 @@
 #include "TextRenderer.h"
+#include "fields/Estate.h"
 
 TextRenderer::TextRenderer(std::shared_ptr<sf::RenderWindow> win)
     : window_(win) {
@@ -39,7 +40,7 @@ void TextRenderer::renderText(const std::string& text, const sf::Vector2f& posit
     //window_.draw(text_);
 }
 
-void TextRenderer::renderPlayers(std::vector<std::shared_ptr<Player>> players, int current_player) {
+void TextRenderer::renderPlayers(std::vector<std::shared_ptr<Player>>& players, int current_player) {
     player_text_.setOutlineColor(sf::Color::White);
     player_text_.setOutlineThickness(0.5f);
 
@@ -61,4 +62,50 @@ void TextRenderer::renderPlayers(std::vector<std::shared_ptr<Player>> players, i
         player_text_.move(0.0f, 24.0f);
     }
     player_text_.setPosition(0.0f, 0.0f);
+}
+
+
+void TextRenderer::renderSquaresDescription(std::vector<std::shared_ptr<BoardSquare>>& squares, int selected_property) {
+    std::string str;
+    for (auto square : squares) {
+        fields_text_.setPosition(fields_text_.getPosition() + sf::Vector2f(0.0, 20.0));
+        fields_text_.setString(square->actionField_->getDescription());
+        fields_text_.setFillColor(square->actionField_->color_);
+
+        if (selected_property == square->actionField_->getId()) {
+            sf::RectangleShape highlightRect;
+            highlightRect.setSize(sf::Vector2f(fields_text_.getLocalBounds().width, fields_text_.getLocalBounds().height + 5.0));
+            highlightRect.setFillColor(sf::Color::Yellow);
+            highlightRect.setPosition(fields_text_.getPosition());
+            window_->draw(highlightRect);
+        }
+
+        if (square->actionField_->owned_) {
+            sf::RectangleShape ownerRect;
+            ownerRect.setSize(sf::Vector2f(20.0f, 20.0f));
+            ownerRect.setFillColor(square->actionField_->owner_color_);
+            ownerRect.setPosition(fields_text_.getPosition().x - 25.0f, fields_text_.getPosition().y);
+            window_->draw(ownerRect);
+            auto estate = std::dynamic_pointer_cast<Estate>(square->actionField_);
+            if (estate) {
+                if (estate->getHotels() == 1) {
+                    sf::CircleShape hotelCircle(10.0f);
+                    hotelCircle.setFillColor(sf::Color::Red);
+                    hotelCircle.setPosition(fields_text_.getPosition().x + fields_text_.getLocalBounds().width + 10.0f, fields_text_.getPosition().y);
+                    window_->draw(hotelCircle);
+                }
+                else {
+                    for (int i = 0; i < estate->getHouses(); ++i) {
+                        sf::CircleShape houseCircle(5.0f);
+                        houseCircle.setFillColor(sf::Color::Green);
+                        houseCircle.setPosition(fields_text_.getPosition().x + fields_text_.getLocalBounds().width + 12.0f + i * 15.0f, fields_text_.getPosition().y);
+                        window_->draw(houseCircle);
+                    }
+                }
+            }
+        }
+
+        window_->draw(fields_text_);
+    }
+    fields_text_.setPosition(950.0f, 5.0f);
 }
